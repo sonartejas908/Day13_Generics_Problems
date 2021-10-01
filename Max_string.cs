59c06c5cdfe253aca1c8206b40626478b7bdde56
Tejassonar <sonartejas908@gmail.com>
2021-10-01 15:46:14 +0530

    Getting max string using Compare to method

---
+++ Max_string.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13Assigment
+{
+    class Max_string
+    {
+        public static void Max_stringCheck()
+        {
+            string firstString = "apple";
+            string secondString = "peach";
+            string thirdString = "banana";
+
+
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+                Console.WriteLine("Biggest String is " + firstString);
+            }
+
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                Console.WriteLine("Biggest String is " + secondString);
+            }
+
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                Console.WriteLine("Biggest String is " + thirdString);
+            }
+
+            
+        }
+
+
+    }
+}
