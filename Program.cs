d870a26f1ed1361ca32a069da6c37d0af87e5477
Tejassonar <sonartejas908@gmail.com>
2021-10-01 15:42:30 +0530

    Added main method for execution

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day13Assigment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MaximumNumberCheck.MaximumIntegerNumber();
+            // MaximumNumberCheck.MaxFloat();
+            //Max_string.Max_stringCheck();
+            int[] intArray = { 455, 654, 85, 852, 85 };
+            Max_using_Generics<int> generics = new Max_using_Generics<int>(intArray);
+            generics.PrintMaxValue();
+            double[] doubleArray = { 45.2, 64.2, 63.4, 89.58, 65.84 };
+            Max_using_Generics<double> genericDouble = new Max_using_Generics<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+        }
+    }
+}
